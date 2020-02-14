bb007ac773642d7c064d556f947d740616495015
dkhoanguyen1101 <56743347+dkhoanguyen1101@users.noreply.github.com>
2020-02-14 10:36:46 -0700

    Update map.java

--- src/map.java
+++ src/map.java
@@ -1,7 +1,5 @@
-import java.util.ArrayList;
 
 
-public class map {
   private int row;
   private int collumn;
   private ArrayList<ArrayList<Integer>> twoDList = new ArrayList<ArrayList<Integer>>();
@@ -19,25 +17,43 @@ public class map {
     	}
     }
 
-  protected int[] getPos(int a) {
+  protected int[] getPos(int id) {
 	  int[] position = new int[2];
 	  for(int countRow = 0; countRow <= row; countRow++) {
-		  if(twoDList.get(countRow).lastIndexOf(a) != -1) {
-		  position[0]=(twoDList.get(countRow).lastIndexOf(a));
-		  position[1]=countRow;
-		  return position;
+		  for(int countCol = 0; countCol <= collumn; countCol++) {
+			  if(twoDList.get(countRow).lastIndexOf(id) != -1) {
+				  int copyCol = countCol;
+				  position[0]=copyCol;
+				  int copyRow = countRow;
+				  position[1]=copyRow;
+				  return position;
 		  }
-	  }
+	  }}
 	  return position;
   }
+
   protected void setPos(int id, int rowCoord, int colCoord) {
-	  for(int countRow = 0; countRow <= row; countRow++) {
-		 for(int countCol = 0; countCol <= collumn; countCol++) {
-			 if(twoDList.get(countRow).get(countCol) == id) {
-				 twoDList.get(countRow).set(countCol, 0);
-			 }
-		 }
-	  }
 	  twoDList.get(rowCoord).set(colCoord, id);
   }
+
+  protected boolean isEmpty(int rowCoord, int colCoord) {
+	  return twoDList.get(rowCoord).get(colCoord).equals(0);
   }
+
+  protected boolean legalMove(int id, int rowCoord, int colCoord, int availableMove) {
+	  if (isEmpty(rowCoord, colCoord) == true) {
+		  int[] position = getPos(id);
+		  int originalCol = position[0];
+		  int originalRow = position[1];
+		  int distance = Math.abs(rowCoord - originalRow) + Math.abs(colCoord - originalCol);
+		  return (distance < availableMove);
+	  }
+	  return false;
+  }
+
+  protected void move(int id, int rowCoord, int colCoord, int availableMove) {
+	 if (legalMove( id, rowCoord, colCoord, availableMove) == true) {
+		 setPos(id, rowCoord, colCoord);
+	 }
+  }
+  
