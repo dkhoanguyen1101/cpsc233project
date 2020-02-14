25ecb3a27a11ddc045f9470f3a757340d874b877
dkhoanguyen1101 <56743347+dkhoanguyen1101@users.noreply.github.com>
2020-02-13 20:14:30 -0700

    Create map.java

---
+++ src/map.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+
+
+public class map {
+  private int row;
+  private int collumn;
+  private ArrayList<ArrayList<Integer>> twoDList = new ArrayList<ArrayList<Integer>>();
+
+  protected map(int r, int cl){
+    int copyr = r;
+    row = copyr;
+    int copycl = r;
+    collumn = copycl;
+    for(int countRow = 0; countRow <= row; countRow++) {
+    	twoDList.add(new ArrayList<Integer>());
+    	for(int countCollumn = 0; countCollumn <= collumn; countCollumn++) {
+    		twoDList.get(countRow).add(0);
+    		}
+    	}
+    }
+  
+  protected int[] getPos(int a) {
+	  int[] position = new int[2];
+	  for(int countRow = 0; countRow <= row; countRow++) {
+		  if(twoDList.get(countRow).lastIndexOf(a) != -1) {
+		  position[0]=(twoDList.get(countRow).lastIndexOf(a));
+		  position[1]=countRow;
+		  return position;
+		  }
+	  }
+	  return position;
+  }
+  protected void setPos(int id, int rowCoord, int colCoord) {
+	  for(int countRow = 0; countRow <= row; countRow++) {
+		 for(int countCol = 0; countCol <= collumn; countCol++) {
+			 if(twoDList.get(countRow).get(countCol) == id) {
+				 twoDList.get(countRow).set(countCol, 0);
+			 }
+		 }
+	  }
+	  twoDList.get(rowCoord).set(colCoord, 0);
+  }
+  }
+  
