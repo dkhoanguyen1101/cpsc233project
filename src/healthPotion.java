4d1fdd31f4e520c15735b8ec17e45552f1a6dd10
dkhoanguyen1101 <56743347+dkhoanguyen1101@users.noreply.github.com>
2020-02-21 14:11:34 -0700

    Update healthPotion.java

--- src/healthPotion.java
+++ src/healthPotion.java
@@ -6,6 +6,8 @@ public class healthPotion extends item {
 		public healthPotion ("Health Poison"){
 			upper("Health Poison");
 		}
+		
+		// main should find the character according to id and pass character into this method
 		public void use(Character c) {
 			c.setHealth(c.getHealth + 100);
 		}
