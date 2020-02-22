93eee9b170f0e409abf0be35a49175a9f0edc08a
DerrickCGit <60663297+DerrickCGit@users.noreply.github.com>
2020-02-22 15:01:36 -0700

    made code better
    code is actually functional now (except maybe the item call). all needed now is AI and supplementary functions

--- src/Game.java
+++ src/Game.java
@@ -3,14 +3,17 @@
  * @author Derrick
  *
  */
+import java.util.Scanner;
 public class Game {
 
 	public static void main(String[] args) {
-		map demoMap = new map(6, 10); //make a generic 7x11 map
+		map demoMap = new map(7, 11); //make a generic 7x11 map
 		Character player = new Character();	//generating the generic player character
 		player.setAttack(10);
 		player.setHealth(100);
 		player.setId(1);
+		item healthPot = new healthPotion();
+		player.setItem(healthPot, 0);
 		Character enemy = new Character(); //generating the generic enemy character
 		enemy.setAttack(5);
 		enemy.setHealth(50);
@@ -23,11 +26,30 @@ public class Game {
 		boolean isWin = true;	//boolean used to determine final message
 		//beginning of the actual game
 		//generic methods used for not, will update later
+		Scanner input = new Scanner(System.in);
 		while(isGame) {
 			if(isPlayerAlive) { //player phase
-				//move();		//supplementary move method
-				//attack();	//supplementary attack method
-							//not sure how use item is going to work - will fix later
+				demoMap.toString();
+				System.out.println("Enter coordinates for where you would like to move"); //prompting user to move on map
+				System.out.print("row: ");
+				int xMove = input.nextInt();
+				System.out.println();
+				System.out.print("column: ");
+				int yMove = input.nextInt();
+				move(player,x,y);
+				//when character has moved, character must do action
+				boolean didSomething = false; //boolean to check character had made legal move
+				while(!didSomething) {
+					System.out.println("Enter 1 to attack"); //use prompts for action
+					System.out.println("Enter 2 to use an item");
+					System.out.println("Enter 3 to end turn now");
+					int choice = input.nextInt();
+					switch(choice) {
+					case 1: didSomething = attack(player); break; //supplementary methods will determine if legal action was taken
+					case 2: didSomething = item(player); break;  //if so, boolean becomes true, moves on to next character
+					case 3: didSomething = true; break; //if no action wants to be taken by user, breaks the loop without acting
+					}
+				}
 				if(enemy.getHealth() <= 0) { //if you killed the enemy
 					isEnemyAlive = false; 	//its dead
 					isGame = false;			//game is over
@@ -35,14 +57,14 @@ public class Game {
 				}
 			}
 			if(isEnemyAlive) { //enemy phase
-				//AI(enemy); //use AI function to determine moves for this character
+				demoMap.toString();
+				AI(enemy); //use AI function to determine moves for this character
 				if(player.getHealth() <= 0) { //if the enemy killed you
 					isPlayerAlive = false;	//you are dead
 					isGame = false;
 					isWin = false;			//you lost
 				}
 			}
-			break; //temporary break so I can save this code in eclipse
 		}
 		if(isWin) { //end of demo message
 			System.out.println("Congratulations you won!");
