60ee65db095455e1ee9e9ae86cd63038a11de2a5
dkhoanguyen1101 <56743347+dkhoanguyen1101@users.noreply.github.com>
2020-03-03 16:55:09 -0700

    static id

--- Item.java
+++ Item.java
@@ -2,12 +2,16 @@
 
 public class Item {
 private  int ID;
+private int staticID;
 private String name;
 public Item(int id, String aName) {
 	setID(id);
 	setName(aName);
 }
-
+public String getStatic() {
+	int toReturn = staticID;
+	return toReturn;
+}
 public String getName() {
 	String toReturn = name;
 	return toReturn;
--- mapGenerator.java
+++ mapGenerator.java
@@ -1,4 +1,4 @@
 
 public class mapGenerator {
-
+private intp[]
 }
