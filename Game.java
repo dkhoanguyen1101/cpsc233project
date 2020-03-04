190c60818759a1cb7b93fffc203b6150a7553884
DerrickCGit <60663297+DerrickCGit@users.noreply.github.com>
2020-03-04 11:05:11 -0700

    Fat load of code
    not done- doesnt even work yet. just posting here

--- Game.java
+++ Game.java
@@ -1,207 +1,185 @@
-/**game runner portion - uses main to facilitate gameplay
+/**controller - facilitates gameplay
  *
  * @author Derrick
  *
  */
 import java.util.Scanner;
+import java.math.*;
+import java.util.ArrayList;
 public class Game {
 
+	private static Scanner userIn = new Scanner(System.in);
+	private static mapGenerator genMap = new mapGenerator();
+	
 	public static void main(String[] args) {
-		map demoMap = new map(7, 11); //make a generic 7x11 map
-		Chara player = new Chara("Player", 1, 10, 100);	//generating the generic player character
-		player.setItem("Health Potion", 0);
-		Chara enemy = new Chara("Enemy", 2, 5, 50); //generating the generic enemy character
-		demoMap.setPos(1, 4, 10); //putting player on right side of map
-		demoMap.setPos(2, 4, 0); //putting enemy on left side of map
-		boolean isPlayerAlive = true;	//booleans used to execute phase if not dead
-		boolean isEnemyAlive = true;
-		boolean isGame = true;	//boolean used to keep turns going is nobody is dead
-		boolean isWin = true;	//boolean used to determine final message
-		//beginning of the actual game
-		//generic methods used for not, will update later
-		Scanner input = new Scanner(System.in);
-		while(isGame) {
-			if(isPlayerAlive) { //player phase
-				System.out.println(demoMap.toString());
-				charToString(player);
-				charToString(enemy);
-				Move(player, demoMap);
-				//when character has moved, character must do action
-				System.out.println(demoMap.toString());
-				boolean didSomething = false; //boolean to check character had made legal move
-				while(!didSomething) {
-					System.out.println("Enter 1 to attack"); //use prompts for action
-					System.out.println("Enter 2 to use an item");
-					System.out.println("Enter 3 to end turn now");
-					int choice = input.nextInt();
-					switch(choice) {
-					case 1: didSomething = Attack(player, enemy, demoMap); break; //supplementary methods will determine if legal action was taken
-					case 2: didSomething = Item(player); break;  //if so, boolean becomes true, moves on to next character
-					case 3: didSomething = true; break; //if no action wants to be taken by user, breaks the loop without acting
+		playGame();
+	}
+	
+	private static void playGame() {
+		map[] worldMap = {genMap.genRandomMap(), genMap.genRandomMap(), genMap.genRandomMap()};
+		ArrayList<Chara> players = new ArrayList<Chara>();
+		ArrayList<Chara> enemies; 
+		boolean stillAlive = true;
+		for(map Map: worldMap) {
+			if(stillAlive) {
+				for(int i = 0; i < players.length; i++) players.get(i).setHealth(players.get(i).getBaseHealth());
+				stillAlive = playMap(players, enemies, Map);
+			}
+		}
+		if (stillAlive) System.out.println("You won");
+		else System.out.println("You lost");
+	}
+	
+	private static boolean playMap(ArrayList<Chara> players, ArrayList<Chara> enemies, map currentMap) {
+		int turnCounter = 0;
+		AI enemyAI = new AI(currentMap);
+		boolean playersAlive = players.size() > 0, enemiesAlive = enemies.size() > 0;
+		while(playersAlive && enemiesAlive) {
+			if(playersAlive) {
+				playerTurn(players, enemies, currentMap);
+			}
+			if(enemiesAlive) {
+				for(int i = 0; i < enemies.length(); i++) {
+					Enemy currentEnemy = enemies.size(i);
+					ArrayList<int[]> playersPos = new ArrayList<int[]>();
+					for(int j = 0; j < players.length; j++) playersPos.add(currentMap.getPos(players.get(i).getUniqueID)); 
+					if (currentEnemy.getNature() == 'A') enemyAI.moveAITowards(currentEnemy.getUniqueID(), playersPos);
+					else if (currentEnemy.getNature() == 'P') enemyAI.moveAIAway(currentEnemy.getUniqueID(), playersPos);
+					Chara closestPlayer = getCharaFromID(enemyAI.checkClosest(currentEnemy.getUniqueID(), playersPos), players);
+					if (isLegalAttack(closestPlayer, currentEnemy)) currentEnemy.attack(closestPlayer);
+				}
+			}
+			playersAlive = players.size() > 0;
+			enemiesAlive = enemies.size() > 0;
+		}
+		return playersAlive;
+	}
+
+	private static void playerTurn(ArrayList<Chara> players, ArrayList<Chara> enemies, map currentMap) {
+		ArrayList<Chara> playersDidntMove = new ArrayList<Chara>(players);
+		ArrayList<Chara> playersDidntAct = new ArrayList<Chara>(players);
+		boolean notAllMoved = playersDidntMove.size() > 0, notAllActed = playersDidntAct.length() > 0;
+			while(notAllMoved || notAllActed) {
+				if(notAllMoved) System.out.println("Enter 1 to move a character");
+				if(notAllActed) System.out.println("Enter 2 to have a character do something");
+				int choice = userIn.nextInt();
+				if(notAllMoved && choice == 1) {
+					System.out.println("These characters can move: ");
+					for(int i = 0; i < playersDidntMove.size(); i++) {
+						System.out.print("ID: " + playersDidntMove.get(i).getUniqueID());
+					} System.out.println();
+					System.out.println("Enter the ID of a character to move");
+					int moveChoice = userIn.nextInt();
+					if(checkIfIDInList(moveChoice, playersDidntMove)) {
+						boolean moved = false;
+						moved = doMove(getCharaFromList(moveChoice, players));
+						if (moved) playersDidntMove.remove(getCharaFromList(moveChoice, playersDidntMove));
 					}
 				}
-				if(enemy.getHealth() <= 0) { //if you killed the enemy
-					isEnemyAlive = false; 	//its dead
-					isGame = false;			//game is over
-					isWin = true;			//you won
+				if(notAllActed && choice == 2) {
+					System.out.println("These characters can act");
+					for(int i = 0; i < playersDidntAct.size(); i++) {
+						System.out.print(" ID: " + playersDidntAct.get(i).getUniqueID());
+					} System.out.println();
+					System.out.println("Enter the ID of a character to act");
+					int actCharChoice = userIn.nextInt();
+					if(checkIfIDInList(actCharChoice, playersDidntAct)) {
+						boolean acted = false;
+						System.out.println("Enter 1 to attack an enemy");
+						System.out.println("Enter 2 to use an item");
+						int actChoice = userIn.nextInt();
+						if(actChoice == 1) {
+							acted = doAttack(getCharaFromID(actCharChoice, players), currentMap);
+						} else if(actChoice == 2) {
+							acted = useItem(getCharaFromID(actCharChoice, players));
+						}
+						if (acted) playersDidntAct.remove(getCharaFromList(actCharChoice, playersDidntAct));
+					}
 				}
+				notAllMoved = playersDidntMove.size() > 0;
+				notAllActed = playersDidntAct.size() > 0;
 			}
-			if(isEnemyAlive) { //enemy phase
-				System.out.println(demoMap.toString());
-				charToString(player);
-				charToString(enemy);
-				demoMap.AImove(enemy.getID(),player.getID());
-				int[] selfPos = demoMap.getPos(enemy.getID());
-			  int[] playerPos = demoMap.getPos(player.getID());
-				if(demoMap.calDist(selfPos[0],selfPos[1],playerPos[0],playerPos[1]) == 1){
-					Attack(enemy, player, demoMap);
-				}//use AI function to determine moves for this character
-				if(player.getHealth() <= 0) { //if the enemy killed you
-					isPlayerAlive = false;	//you are dead
-					isGame = false;
-					isWin = false;			//you lost
-				}
-			}
-		}
-		if(isWin) { //end of demo message
-			System.out.println("Congratulations you won!");
-		} else {
-			System.out.println("You lost.");
-		}
 	}
-
-
-	//Attack, Item, and Move methods in the Game class are made by Robert Ferguson.
-
-
-	public static boolean Attack(Chara attacker, Chara receiver, map map) {
-		//check if attacker is in range
-		int attackerX = map.getPos(attacker.getID())[0];
-		int attackerY = map.getPos(attacker.getID())[1];
-		int receiverX = map.getPos(receiver.getID())[0];
-		int receiverY = map.getPos(receiver.getID())[1];
-		//System.out.println("" + attackerX + attackerY + receiverX + receiverY);
-		if (((attackerX == receiverX + 1) || (attackerX == receiverX - 1)) || ((attackerY == receiverY + 1) || (attackerY == receiverY - 1))) {
-			//lower receiver health
-			receiver.setHealth((receiver.getHealth()) - (attacker.getAttack()));
-			System.out.println(attacker.getName() + " attacked " + receiver.getName() + " for " + attacker.getAttack() + " health!");
-			//change boolean to true
-			return true;
-		} else {
-			//if not within range, return false
-			System.out.println(attacker.getName() + " cannot reach anybody!");
-			return false;
-		}
-
-
+	
+	private static boolean doMove(Chara charToMove, map currentMap) {
+		boolean didMove = false;
+		System.out.println("Enter the x coordinate you want to move to");
+		int newX = userIn.nextInt();
+		System.out.println("enter the y coordinate you want to move to");
+		int newY = userIn.nextInt();
+		didMove = currentMap.move(charToMove.getUniqueID, newY, newX, charToMove.getMove());
+		return didMove;
 	}
-
-	public static boolean Item(Chara user) {
-		//check if inventory is empty
-		String slot1 = user.getItem(0);
-		String slot2 = user.getItem(1);
-		String slot3 = user.getItem(2);
-		if (slot1 == "empty" && slot2 == "empty" && slot3 == "empty") {
-			System.out.println(user.getName() + "'s inventory is empty");
-			return false;
+	
+	private static boolean doAttack(Chara attacker, ArrayList<Chara> enemiesList, map currentMap) {
+		System.out.println("You can attack in a range of " + attacker.getAtkRange());
+		System.out.println("Enter the ID of an enemy to attack");
+		int choice = userIn.nextInt();
+		Chara receiver = getCharaFromID(choice, enemiesList);
+		boolean didAttack = false;
+		if (checkIfIDInList(choice, enemiesList)) {
+			if (isLegalAttack(attacker, receiver, currentMap)) {
+				attacker.attack(receiver);
+				didAttack = true;
+			}
 		} else {
-			//ask for user input
-			Scanner sc = new Scanner(System.in);
-			System.out.println("Select an Item");
-			System.out.println("type 1 ("+ slot1 +"), 2 ("+ slot2 +"), or 3 ("+ slot3 +")");
-			int select = sc.nextInt();
-			if (select == 1) {
-				if (slot1.equals("Health Potion")) {
-					//if input correct, make item, use item and change string to empty
-					//healthPotion hPotion = new item().new healthPotion();
-					//hPotion.setAttributes();
-					//hPotion.characterHealth(user.getID());
-					user.setHealth(user.getHealth() + 50);
-					System.out.println("Restored 50 health!");
-					//make method that lets me change specific slots
-					user.setItem("empty", 0);
-
-
-					//change boolean to true
-					return true;
-				} else {
-					System.out.println("That slot is empty");
-					return false;
-				}
-			} else if (select == 2) {
-				if (slot2.equals("Health Potion")) {
-					//if input correct, make item, use item and change string to empty
-					//healthPotion hPotion = new item().new healthPotion();
-					//hPotion.setAttributes();
-					//hPotion.characterHealth(user.getID());
-					user.setHealth(user.getHealth() + 50);
-					System.out.println("Restored 50 health!");
-					//make method that lets me change specific slots
-					user.setItem("empty", 1);
-
-
-					//change boolean to true
-					return true;
-				} else {
-					System.out.println("That slot is empty");
-					return false;
-				}
-			} else if (select == 3) {
-				if (slot3.equals("Health Potion")) {
-					//if input correct, make item, use item and change string to empty
-					//healthPotion hPotion = new item().new healthPotion();
-					//hPotion.setAttributes();
-					//hPotion.characterHealth(user.getID());
-					user.setHealth(user.getHealth() + 50);
-					System.out.println("Restored 50 health!");
-
-					//make method that lets me change specific slots
-					user.setItem("empty", 2);
-
-
-					//change boolean to true
-					return true;
-				} else {
-					System.out.println("That slot is empty");
-					return false;
-				}
-
-			} else {
-				//send back to menu if item is not selected
-				System.out.println("That is not a valid item");
-				return false;
-			}
+			System.out.println("That enemy does not exist");
 		}
+		return didAttack;
 	}
-
-	//moves chosen character to new location on map, provided location is legal
-
-	public static void Move(Chara chara, map map) {
-		//ask for user input (x and y positions)
-		Scanner pick = new Scanner(System.in);
-		System.out.println("Enter 1 to move or anything else to stay put");
-		int stay = pick.nextInt();
-		if(stay == 1) {
-			System.out.println("Pick a y position to move to");
-			int newX = pick.nextInt();
-			System.out.println("Now pick an x position to move to");
-			int newY = pick.nextInt();
-			while (map.isLegalMove(chara.getID(), newX, newY, 4) == false) {
-				System.out.println("that is not a legal move");
-				System.out.println("Pick another y position to move to");
-				newX = pick.nextInt();
-				System.out.println("Now pick another x position to move to");
-				newY = pick.nextInt();
-			}
-			//once positions are legal, move to that position
-			map.move(chara.getID(), newX, newY, 4);
+	
+	private static boolean isLegalAttack(Chara attacker, Chara receiver, map currentMap) {
+		int atkRange = attacker.getAtkRange();
+		int[] atkPos = currentMap.getPos(attacker.getUniqueID());
+		int[] recPos = currentMap.getPos(receiver.getUniqueID());
+		int atkDist = (Math.abs(atkPos[0] - recPos[0]) + Math.abs(atkPos[1] - recPos[1]));
+		if (atkDist > atkRange) return false;
+		else return true;
+	}
+	
+	public static void kill(Chara charToKill, ArrayList<Chara> listContainingChar, map currentMap) {
+		int[] killCoords = getPos(charToKill.getUniqueID());
+		currentMap.setPos(0, killCoords[0], killCoords[1]);
+		listContainingChar.remove(charToKill);
+	}
+	
+	private static boolean useItem(Chara user) {
+		System.out.println("Enter a number to use item");
+		for (int i = 0, i < 3, i++) {
+			System.out.println("" + (i + 1) + ". " + user.getItem(i).getName());
 		}
-		//ask for new x and y position if position is illegal
+		System.out.println("4. Do not use an item");
+		int choice = userIn.nextInt();
+		boolean itemWasUsed = false;
+		switch(choice) {
+		case 1:
+			itemWasUsed: user.getItem(0).use(); break;
+		case 2:
+			itemWasUsed: user.getItem(1).use(); break;
+		case 3:
+			itemWasUsed: user.getItem(2).use(); break;
+		default:
+			itemWasUsed = false;
+		}
+		if (itemWasUsed) user.setItem(choice - 1, new emptyItem());
 		
+		return itemWasUsed;
 	}
-
-
-	public static void charToString(Chara character) {
-		System.out.println(character.getName() + "(ID: " + character.getID() +"): " + character.getHealth() + " health");
+	
+	public static Chara getCharaFromID(int ID, ArrayList<Chara> listToCheck) {
+		Chara toReturn = null;
+		for(int i = 0; i < listToCheck.size(); i++) {
+			if (listToCheck.get(i).getUniqueID() == ID) toReturn = listToCheck.get(i);
+		}
+		return toReturn;
 	}
-}
+	
+	public static boolean checkIfIDInList(int ID, ArrayList<Chara> listToCheck) {
+		boolean isThere = false;
+		for(int i = 0; i < listToCheck.size(); i++) {
+			if (!isThere) {
+				if (listToCheck.get(i).getUniqueID() == ID) isThere = true;
+			}
+		}
+		return isThere;
+	}
