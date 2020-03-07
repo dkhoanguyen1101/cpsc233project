fb8a40a1a8b0e10bef37c1946e09afe3b857e6ad
DerrickCGit <60663297+DerrickCGit@users.noreply.github.com>
2020-03-07 14:14:58 -0700

    moving mana to chara

--- EngMajor.java
+++ EngMajor.java
@@ -4,22 +4,10 @@ import java.util.Scanner;
 public class EngMajor extends Chara{
 	Scanner userIn = new Scanner(System.in);
 	
-	private int mana, maxMana;
 	public EngMajor(String name, int id, int att, int health, int mana, 
 			int maxAtt, int maxHealth, int maxMana, int move, int range) {
-		super(name, id, att, health, maxAtt, maxHealth, move, range);
-		setMaxMana(maxMana);
-		setMana(mana);
+		super(name, id, att, health, mana, maxAtt, maxHealth, maxMana, move, range);
 	}
-	
-	public int getMana() {return mana;}
-	public void setMana(int newMana) {
-		if(newMana >= maxMana) mana = maxMana;
-		else mana = newMana;
-	}
-	
-	public int getMaxMana() {return maxMana;}
-	public void setMaxMana(int newMax) {maxMana = newMax;}
 	
 	public boolean Special(map theMap, ArrayList<Chara> players, ArrayList<Chara> enemies) {
 		boolean didSomething = false;
