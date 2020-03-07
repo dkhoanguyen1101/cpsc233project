49289cae34648340a848c19ebb2488ff605994a8
DerrickCGit <60663297+DerrickCGit@users.noreply.github.com>
2020-03-07 14:04:42 -0700

    mana checks

--- KinesiologyMajor.java
+++ KinesiologyMajor.java
@@ -23,34 +23,37 @@ public class KinesiologyMajor extends Chara{
 	
 	public boolean Special(map theMap, ArrayList<Chara> players, ArrayList<Chara> enemies) {
 		boolean didSomething = false;
-		System.out.println("Attack an enemy for 2x damage within 1 cell");
-		System.out.println("Enter the ID of an enemy to attack");
-		int choice = userIn.nextInt();
-		boolean isEnemy = false;
-		int enemyID = 0;
-		int enemyIndex = 0;
-		for(int i = 0; i < enemies.size(); i++) {
-			if(!isEnemy) {
-				enemyID = enemies.get(i).getID();
-				isEnemy = enemyID == choice;
-				enemyIndex = i;
+		if(mana < 2) System.out.println("This special requires 2 mana");
+		else {
+			System.out.println("Attack an enemy for 2x damage - within 1 cell (costs 2 mana)");
+			System.out.println("Enter the ID of an enemy to attack");
+			int choice = userIn.nextInt();
+			boolean isEnemy = false;
+			int enemyID = 0;
+			int enemyIndex = 0;
+			for(int i = 0; i < enemies.size(); i++) {
+				if(!isEnemy) {
+					enemyID = enemies.get(i).getID();
+					isEnemy = enemyID == choice;
+					enemyIndex = i;
+				}
 			}
-		}
-		if(isEnemy) {
-			int[] enemyPos = theMap.getPos(enemyID);
-			int[] playerPos = theMap.getPos(getID());
-			int range = Math.abs((enemyPos[0] - playerPos[0]) + (enemyPos[1] - playerPos[1]));
-			if(range <= 1) {
-				int regAttack = getAttack();
-				setAttack(regAttack * 2);
-				attack(enemies.get(enemyIndex));
-				setAttack(regAttack);
-				didSomething = true;
+			if(isEnemy) {
+				int[] enemyPos = theMap.getPos(enemyID);
+				int[] playerPos = theMap.getPos(getID());
+				int range = Math.abs((enemyPos[0] - playerPos[0]) + (enemyPos[1] - playerPos[1]));
+				if(range <= 1) {
+					int regAttack = getAttack();
+					setAttack(regAttack * 2);
+					attack(enemies.get(enemyIndex));
+					setAttack(regAttack);
+					didSomething = true;
+				} else {
+					System.out.println("Enemy is not in range");
+				}
 			} else {
-				System.out.println("Enemy is not in range");
+				System.out.println("That enemy does not exist");
 			}
-		} else {
-			System.out.println("That enemy does not exist");
 		}
 		return didSomething;
 	}
