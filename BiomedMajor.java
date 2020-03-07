f1a2aad700ad6fe9d4d742bc569bc291c5bd2b58
DerrickCGit <60663297+DerrickCGit@users.noreply.github.com>
2020-03-07 14:10:41 -0700

    mana checks

--- BiomedMajor.java
+++ BiomedMajor.java
@@ -25,33 +25,37 @@ public class BiomedMajor extends Chara {
 	
 	public boolean Special(map theMap, ArrayList<Chara> players, ArrayList<Chara> enemies) {
 		boolean didSomething = false;
-		System.out.println("Heal another teammate for 40 health - within 3 spaces");
-		System.out.println("Enter the ID of a teammate to heal");
-		Integer choice = userIn.nextInt();
-		boolean isPlayer = false;
-		int playerID = 0;
-		int playerIndex = 0;
-		for(int i = 0; i < enemies.size(); i++) {
-			if(!isPlayer) {
-				playerID = enemies.get(i).getID();
-				isPlayer = playerID == choice;
-				playerIndex = i;
+		if(mana < 3) System.out.println("This special requires 3 mana");
+		else {
+			System.out.println("Heal another teammate for 40 health - within 3 spaces (costs 3 mana)");
+			System.out.println("Enter the ID of a teammate to heal");
+			Integer choice = userIn.nextInt();
+			boolean isPlayer = false;
+			int playerID = 0;
+			int playerIndex = 0;
+			for(int i = 0; i < enemies.size(); i++) {
+				if(!isPlayer) {
+					playerID = enemies.get(i).getID();
+					isPlayer = playerID == choice;
+					playerIndex = i;
+				}
 			}
-		}
-		
-		if(isPlayer) {
-			int[] playerPos = theMap.getPos(playerID);
-			int[] healerPos = theMap.getPos(getID());
-			int range = Math.abs((healerPos[0] - playerPos[0]) + (healerPos[1] - playerPos[1]));
-			if(range <= 3) {
-				Chara healed = players.get(playerIndex);
-				healed.setHealth(healed.getHealth() + 40);
-				didSomething = true;
+			
+			if(isPlayer) {
+				int[] playerPos = theMap.getPos(playerID);
+				int[] healerPos = theMap.getPos(getID());
+				int range = Math.abs((healerPos[0] - playerPos[0]) + (healerPos[1] - playerPos[1]));
+				if(range <= 3) {
+					Chara healed = players.get(playerIndex);
+					healed.setHealth(healed.getHealth() + 40);
+					setMana(getMana() - 3);
+					didSomething = true;
+				} else {
+					System.out.println("Teammate is not in range");
+				}
 			} else {
-				System.out.println("Teammate is not in range");
+				System.out.println("That teammate does not exist");
 			}
-		} else {
-			System.out.println("That teammate does not exist");
 		}
 		return didSomething;
 	}
