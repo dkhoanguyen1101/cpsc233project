2be459d79900ae3f6ab0422e2635364e7c18ab7f
DerrickCGit <60663297+DerrickCGit@users.noreply.github.com>
2020-03-07 14:14:12 -0700

    moving mana to chara

--- BiomedMajor.java
+++ BiomedMajor.java
@@ -6,7 +6,6 @@ public class BiomedMajor extends Chara {
 	
 	Scanner userIn = new Scanner(System.in);
 	
-	private int mana, maxMana;
 	public BiomedMajor(String name, int id, int att, int health, int mana, 
 			int maxAtt, int maxHealth, int maxMana, int move, int range) {
 		super(name, id, att, health, mana, maxAtt, maxHealth, maxMana, move, range);
