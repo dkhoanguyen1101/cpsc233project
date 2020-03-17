797d78e52c435da8bc6fc2f8e0a961dda6bd9a08
robert618 <59930386+robert618@users.noreply.github.com>
2020-03-17 13:20:38 -0600

    Update MovementPotion.java

--- text-based/MovementPotion.java
+++ text-based/MovementPotion.java
@@ -5,8 +5,10 @@ public class MovementPotion extends Item{
 		super(id, aName);
 	}
 	
+	//increases the users movement by two permenently
 	public boolean use(Chara c) {
-		return false;
+		c.setMove(c.getMove() + 2);
+		return true;
 	}
 }
 
