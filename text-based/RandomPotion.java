74e866e437397ee0d3c9db327bf354d150074c04
robert618 <59930386+robert618@users.noreply.github.com>
2020-03-17 13:09:41 -0600

    Update RandomPotion.java

--- text-based/RandomPotion.java
+++ text-based/RandomPotion.java
@@ -5,7 +5,13 @@ public class RandomPotion extends Item{
 		super(id, aName);
 	}
 	
+	//RandomPotion raises the Health and Mana of the user by a random amount
 	public boolean use(Chara c) {
-		return false;
+		Random rand = new Random();
+		int hlthGain = rand.nextInt(15);
+		int manaGain = rand.nextInt(3);
+		c.setHealth(c.getHealth() + (hlthGain * 10));
+		c.setMana(c.getMana() + manaGain);
+		return true;
 	}
 }
