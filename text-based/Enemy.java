0e719462d8cdc5a05e6f1c1529594f960f6e698a
robert618 <59930386+robert618@users.noreply.github.com>
2020-03-17 13:14:56 -0600

    Update Enemy.java

--- text-based/Enemy.java
+++ text-based/Enemy.java
@@ -1,15 +1,17 @@
-	
-	import java.util.ArrayList;
-	import java.util.Scanner;
-	public class Enemy extends Chara{
-		Scanner userIn = new Scanner(System.in);
+import java.util.ArrayList;
+
+public class Enemy extends Chara{
 		
-		public Enemy(String name, int id, int att, int health, int mana, 
-				int maxAtt, int maxHealth, int maxMana, int move, int range) {
-			super(name, id, att, health, mana, maxAtt, maxHealth, maxMana, move, range);
-		}
+	public Enemy(String name, int id, int att, int health, int mana, 
+			int maxAtt, int maxHealth, int maxMana, int move, int range) {
+		super(name, id, att, health, mana, maxAtt, maxHealth, maxMana, move, range);
+	}
 		
-		public boolean Special(map theMap, ArrayList<Chara> players, ArrayList<Chara> enemies) {
-			return false;
-		}
+	/* basic enemies do not have specials.
+	 * instead, they simply return that the special 
+	 * cannot be used at all times	*/
+		
+	public boolean Special(map theMap, ArrayList<Chara> players, ArrayList<Chara> enemies) {
+		return false;
+	}
 }
