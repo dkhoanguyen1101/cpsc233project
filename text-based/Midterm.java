1842e17a723a2bec65649c7e8dd5f9bb01ffa972
robert618 <59930386+robert618@users.noreply.github.com>
2020-03-17 13:19:10 -0600

    Update Midterm.java

--- text-based/Midterm.java
+++ text-based/Midterm.java
@@ -1,18 +1,27 @@
+		
+import java.util.ArrayList;
+public class Midterm extends Chara {
+		
+	public Midterm(String name, int id, int att, int health, int mana, 
+			int maxAtt, int maxHealth, int maxMana, int move, int range) {
+		super(name, id, att, health, mana, maxAtt, maxHealth, maxMana, move, range);
+	}
+
+	//this special is a high-cost, but permanent-debuff on all players
 	
-	import java.util.ArrayList;
-	import java.util.Scanner;
-	public class Midterm extends Chara{
-		Scanner userIn = new Scanner(System.in);
+	public boolean Special(map theMap, ArrayList<Chara> players, ArrayList<Chara> enemies) {
+		boolean didSomething = false;
+		//check to make sure there is enough mana to use ability (10)
+		if(getMana() >= 10)
+			//reduce all player characters attack value by one
+			for(Chara member : players) {		
+					member.setAttack(member.getAttack() - 1);
+				//lower mana value by ten
+				setMana(getMana() - 10);
+				}
+				//mark that special was used
+				didSomething = true;
 		
-		public Midterm(String name, int id, int att, int health, int mana, 
-				int maxAtt, int maxHealth, int maxMana, int move, int range) {
-			super(name, id, att, health, mana, maxAtt, maxHealth, maxMana, move, range);
-		}
-		
-		/*this special is a high-cost, but permanent-debuff on the targeted player
-		*/
-		public boolean Special(map theMap, ArrayList<Chara> players, ArrayList<Chara> enemies) {
-			
-			return false;
-		}
+		return didSomething;
+	}
 }
