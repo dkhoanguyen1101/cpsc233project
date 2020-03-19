673c3b98371b575d3f0c7b8f44781bdd80baa0d9
robert618 <59930386+robert618@users.noreply.github.com>
2020-03-19 14:31:17 -0600

    Update EngMajor.java

--- text-based/EngMajor.java
+++ text-based/EngMajor.java
@@ -1,14 +1,30 @@
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class EngMajor extends Chara{
-	Scanner userIn = new Scanner(System.in);
+public class EngMajor extends Chara {
+
 	
-	public EngMajor(String name, int id, int att, int health, int mana, 
-			int maxAtt, int maxHealth, int maxMana, int move, int range) {
-		super(name, id, att, health, mana, maxAtt, maxHealth, maxMana, move, range);
-	}
+	//instance variables
+		Scanner userIn = new Scanner(System.in);
+		private static int maxHealth = 200;
+		private static int maxAttack = 20;
+		private static int maxMana = 5;
+		private int ID;
+		private static String name = "Engineering Major";
+		private Item[] inventory = { new HealthPotion(),  new EmptyItem(),  new EmptyItem()};	
+		
+		//constructor
+		public EngMajor(int id) {
+			this.ID = id;
+			this.setAttack(maxAttack);
+			this.setHealth(maxHealth);
+			this.setMana(3);
+			this.setRange(4);
+			this.setMove(4);
+		}
 	
+	//this special attacks any enemies in the same row as the engineer
 	public boolean Special(map theMap, ArrayList<Chara> players, ArrayList<Chara> enemies) {
 		boolean didSomething = false;
 		if(getMana() < 2) System.out.println("This special requires 2 mana");
@@ -29,4 +45,3 @@ public class EngMajor extends Chara{
 		}
 		return didSomething;
 	}
-}
