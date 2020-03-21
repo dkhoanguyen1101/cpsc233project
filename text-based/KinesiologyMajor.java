dd9a6451849de17dd9d9acad2a3ebd05d1692c21
robert618 <59930386+robert618@users.noreply.github.com>
2020-03-20 21:44:47 -0600

    Update KinesiologyMajor.java

--- text-based/KinesiologyMajor.java
+++ text-based/KinesiologyMajor.java
@@ -1,7 +1,6 @@
 
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.math.*;
 
 public class KinesiologyMajor extends Chara {
 
@@ -10,19 +9,17 @@ public class KinesiologyMajor extends Chara {
 			
 	//constructor
 	public KinesiologyMajor(int id) {
-		super("KinesiologyMajor", id, 30, 250, 3, 30, 250, 5, 4, 1, "This special deals a chosen near-by enemy a large amount of damage (costs 2 mana)");
+		super("KinesiologyMajor", id, 30, 250, 3, 30, 250, 5, 4, 1, "This special deals a chosen near-by enemy a large amount of damage (costs 2 mana). Select the tile of the enemy you want to attack.");
 			}
 	
 	//methods
 	
 	//this special deals a close-by enemy a large amount of damage
-	public boolean Special(map theMap, ArrayList<Chara> players, ArrayList<Chara> enemies) {
+	public boolean Special(map theMap, ArrayList<Chara> players, ArrayList<Chara> enemies, int xPos, int yPos) {
 		boolean didSomething = false;
 		if(getMana() < 2) System.out.println("This special requires 2 mana");
 		else {
-			System.out.println("Attack an enemy for 2x damage - within 1 cell (costs 2 mana)");
-			System.out.println("Enter the ID of an enemy to attack");
-			int choice = userIn.nextInt();
+			int choice = theMap.getID(xPos, yPos);
 			boolean isEnemy = false;
 			int enemyID = 0;
 			int enemyIndex = 0;
