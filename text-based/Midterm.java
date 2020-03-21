c01186bd475e847588b6bafaa0c7d75c666c6807
robert618 <59930386+robert618@users.noreply.github.com>
2020-03-20 21:45:20 -0600

    Update Midterm.java

--- text-based/Midterm.java
+++ text-based/Midterm.java
@@ -4,12 +4,12 @@ public class Midterm extends Chara {
 		
 	public Midterm(String name, int id, int att, int health, int mana, 
 			int maxAtt, int maxHealth, int maxMana, int move, int range) {
-		super(name, id, att, health, mana, maxAtt, maxHealth, maxMana, move, range, "none");
+		super(name, id, att, health, mana, maxAtt, maxHealth, maxMana, move, range, "");
 	}
 
 	//this special is a high-cost, but permanent-debuff on all players
 	
-	public boolean Special(map theMap, ArrayList<Chara> players, ArrayList<Chara> enemies) {
+	public boolean Special(map theMap, ArrayList<Chara> players, ArrayList<Chara> enemies, int xPos, int yPos) {
 		boolean didSomething = false;
 		//check to make sure there is enough mana to use ability (10)
 		if(getMana() >= 10)
