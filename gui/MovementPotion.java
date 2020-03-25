4e2651b5f4894e77efc7b86ffa22703b99d9ef87
robert618 <59930386+robert618@users.noreply.github.com>
2020-03-24 19:37:46 -0600

    Update MovementPotion.java

--- gui/MovementPotion.java
+++ gui/MovementPotion.java
@@ -1,8 +1,8 @@
 
 public class MovementPotion extends Item{
 
-	public MovementPotion(int id, String aName) {
-		super(id, aName);
+	public MovementPotion(int id) {
+		super(id, "Movement Potion");
 	}
 	
 	//increases the users movement by two permenently
