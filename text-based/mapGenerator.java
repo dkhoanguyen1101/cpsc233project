75b919b46fefa1c0a2b2c7ab428c17b5bb51f022
dkhoanguyen1101 <56743347+dkhoanguyen1101@users.noreply.github.com>
2020-03-25 16:56:25 -0600

    walls

--- text-based/mapGenerator.java
+++ text-based/mapGenerator.java
@@ -3,9 +3,9 @@ import java.util.Random;
 public class mapGenerator {
 	
 map toReturn;
-private int[][] map1 = {{0, 13, 1, 14, 2, 15}, {13, 0, 14, 1, 15,2}, {4,11}};
-private int[][] map2 = { {13, 0, 14, 1, 15,2},{0, 13, 1, 14, 2, 15}, {11,4}};
-private int[][] map3 = { {6, 11, 7, 11, 8,11},{6, 4, 7, 4, 8,4}, {11,4}};
+private int[][] map1 = {{2, 5, 3, 5, 4, 5}, {11, 10, 12, 10, 13,10}, {7,7}};
+private int[][] map2 = { {4, 5, 4, 7, 4,9},{11, 5, 11, 7, 11, 9}, {7,7}};
+private int[][] map3 = { {5, 5, 7, 5, 9,5},{5, 10, 7, 10, 9,10}, {7,7}};
 public mapGenerator() {
 	generate();
 	}
@@ -19,19 +19,85 @@ public map generate() {
 	int mapNum = numberGen.nextInt(3) +1;
 	switch(mapNum) {
 	case( 1):{
+		
 		map toReturn = new map(map1[0], map1[1], map1[2]);
+		for(int i = 0; i < 5; i++) {
+			for(int j = 0; j < 5; j++) {
+				toReturn.setPos(-1, j, i);
+			}
+		}
+		
+		for(int i = 11; i < 16; i++) {
+			for(int j = 11; j < 16; j++) {
+				toReturn.setPos(-1, j, i);
+			}
+		}
 		return toReturn;
 	}
 	case( 2):{
+		
 		map toReturn = new map(map2[0], map2[1], map2[2]);
+		
+		for(int i = 0; i<5; i++) {
+			for(int j = 0; j < 5-i;j++) {
+				toReturn.setPos(-1, j, i);
+			}
+		}
+		for(int i = 0; i<5; i++) {
+			for(int j = 15;j  > 10 + i;j--) {
+				toReturn.setPos(-1, j, i);
+			}
+		}
+		
+		for(int i = 0; i<5; i++) {
+			for(int j = 15; j > 10 + i;j--) {
+				toReturn.setPos(-1, j, i);
+			}
+		}
+		
+		for(int i = 15; i>10; i--) {
+			for(int j = 0; j < i - 10;j++) {
+				toReturn.setPos(-1, j, i);
+			}
+		}
+		
+		for(int i = 15; i>10; i--) {
+			for(int j = 15; j > 25-i;j--) {
+				toReturn.setPos(-1, j, i);
+			}
+		}
+
 		return toReturn;
 	}
 	case( 3):{
+		System.out.print(3);
 		map toReturn = new map(map3[0], map3[1], map3[2]);
+		for(int i = 0; i < 5 ; i++) {
+			for (int j = i; j<16 - i; j ++) {
+				toReturn.setPos(-1, j, i);
+			}
+		}
+		
+		for(int i = 15; i > 10 ; i--) {
+			for (int j = i; j >= 15-i; j --) {
+				toReturn.setPos(-1, j, i);
+			}
+		}
 		return toReturn;
 	}
 	default:{
 		map toReturn = new map(map1[0], map1[1], map1[2]);
+		for(int i = 0; i < 5; i++) {
+			for(int j = 0; j < 5; j++) {
+				toReturn.setPos(-1, j, i);
+			}
+		}
+		
+		for(int i = 11; i < 16; i++) {
+			for(int j = 11; j < 16; j++) {
+				toReturn.setPos(-1, j, i);
+			}
+		}
 		return toReturn;}
 	}
 	
