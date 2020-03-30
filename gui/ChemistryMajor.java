1556f35895fbd48b696cddba1eaba2a22b44fba6
yuwen-liang <60663219+yuwen-liang@users.noreply.github.com>
2020-03-30 12:55:26 -0600

    Update ChemistryMajor.java

--- gui/ChemistryMajor.java
+++ gui/ChemistryMajor.java
@@ -7,8 +7,8 @@ public class ChemistryMajor extends Chara {
 	Scanner userIn = new Scanner(System.in);	
 	
 	//constructor
-	public ChemistryMajor(int id) {
-		super("Chemistry Major", id, 15, 200, 2, 15, 200, 5, 5, 1, "This special boosts the attack of a chosen ally (costs 3 mana). Select the ally you want to boost on the map");
+	public ChemistryMajor(String name, int id, int att, int health, int mana, int maxAtt, int maxHealth, int maxMana, int move, int range, String specDesc, String imageUrl) {
+		super("Chemistry Major", id, 15, 200, 2, 15, 200, 5, 5, 1, "This special boosts the attack of a chosen ally (costs 3 mana). Select the ally you want to boost on the map", "Chemistry.png");
 	}
 
 	//methods
