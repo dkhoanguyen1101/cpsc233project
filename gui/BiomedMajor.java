ceddd26af190f9444a45294a879974fb767761ca
yuwen-liang <60663219+yuwen-liang@users.noreply.github.com>
2020-03-30 12:52:56 -0600

    Update BiomedMajor.java

--- gui/BiomedMajor.java
+++ gui/BiomedMajor.java
@@ -7,8 +7,8 @@ public class BiomedMajor extends Chara {
 	
 	
 	//constructor
-	public BiomedMajor(int id) {
-super("Biomedical Major", id, 10, 250, 3, 10, 250, 7, 4, 1, "This special boosts the health of a chosen nearby ally (costs 3 mana). Select the tile of the teammate you would like to heal that is within three tiles");
+	public BiomedMajor(String name, int id, int att, int health, int mana, int maxAtt, int maxHealth, int maxMana, int move, int range, String specDesc, String imageUrl) {
+        super("Biomedical Major", id, 10, 250, 3, 10, 250, 7, 4, 1, "This special boosts the health of a chosen nearby ally (costs 3 mana). Select the tile of the teammate you would like to heal that is within three tiles", "BioChem.png");
 	}
 	
 	//methods
