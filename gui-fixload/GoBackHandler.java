ffc7968efb96f672e7e15af69cd9a773e792b633
yuwen-liang <60663219+yuwen-liang@users.noreply.github.com>
2020-04-13 14:44:01 -0600

    Update GoBackHandler.java

--- gui-fixload/GoBackHandler.java
+++ gui-fixload/GoBackHandler.java
@@ -7,15 +7,15 @@ import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.control.Label;
-
+//method for go back handler
 public class GoBackHandler implements EventHandler<ActionEvent>{
-	
+	//instance variable
 	Game game;
-	
+	//constructor
 	public GoBackHandler(Game box) {
 		game = box;
 	}
-	
+	//main method for back handler
 	public void handle(ActionEvent event) {
 		game.setButtonTextPlayerActionChoice();
 		game.btn1.setOnAction(null);
