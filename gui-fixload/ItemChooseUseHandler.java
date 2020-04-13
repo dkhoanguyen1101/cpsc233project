4e402ce868d0fe571f471a724e2044904929181a
yuwen-liang <60663219+yuwen-liang@users.noreply.github.com>
2020-04-13 14:44:51 -0600

    Update ItemChooseUseHandler.java

--- gui-fixload/ItemChooseUseHandler.java
+++ gui-fixload/ItemChooseUseHandler.java
@@ -7,17 +7,17 @@ import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.control.Label;
-
+//method for item choose handler
 public class ItemChooseUseHandler implements EventHandler<ActionEvent>{
-	
+	//instance variable
 	Game game;
 	Chara user;
 	int itemSpace;
-	
+	//constructor
 	public ItemChooseUseHandler(Game box, Chara u, int i) {
 		game = box; user = u; itemSpace = i;
 	}
-	
+	//main method for item choose handler
 	public void handle(ActionEvent event) {
 		boolean used = user.getItem(itemSpace).use(user);
 		if(used) {
