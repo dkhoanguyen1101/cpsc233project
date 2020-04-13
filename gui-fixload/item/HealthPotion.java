bea773ae1cda1a3edb391c0dde1e3961da23d0c4
dkhoanguyen1101 <56743347+dkhoanguyen1101@users.noreply.github.com>
2020-04-13 15:20:01 -0600

    packaging

---
+++ gui-fixload/item/HealthPotion.java
@@ -0,0 +1,17 @@
+package item;
+
+import characters.Chara;
+
+public class HealthPotion extends Item {
+
+	public HealthPotion(){
+	super(32, "Health Potion");
+}
+	//increases the users health points by 50
+	public boolean  use(Chara c){
+	c.setHealth(c.getHealth() + 50);
+	return true;
+	
+	
+}
+}
---
+++ gui-fixload/item/Item.java
@@ -0,0 +1,42 @@
+package item;
+import java.io.Serializable;
+
+import characters.Chara;
+
+public abstract class Item implements Serializable {
+private  int ID;
+private int staticID;
+private String name, imageURL;
+public Item(int id, String aName) {
+	setID(id);
+	setName(aName);
+	imageURL = "Item.png";
+}
+public int getStatic() {
+	int toReturn = staticID;
+	return toReturn;
+}
+public String getName() {
+	String toReturn = name;
+	return toReturn;
+}
+public int getID() {
+	int toReturn = ID;
+	return toReturn;
+}
+
+public void setID(int i) {
+	ID = i;
+}
+public void setName(String aName) {
+	name = aName;
+}
+
+public String getImageURL() {
+	return new String(imageURL);
+}
+
+public abstract boolean use(Chara c);
+
+
+}
