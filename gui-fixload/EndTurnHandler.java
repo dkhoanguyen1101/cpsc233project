d12286918ee512e6e094651f1b62237bc8c25579
yuwen-liang <60663219+yuwen-liang@users.noreply.github.com>
2020-04-13 14:43:25 -0600

    Update EndTurnHandler.java

--- gui-fixload/EndTurnHandler.java
+++ gui-fixload/EndTurnHandler.java
@@ -5,11 +5,11 @@ import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.control.Label;
-
+//method for end turn handler
 public class EndTurnHandler implements EventHandler<ActionEvent>{
-
+//constructor
 	Game game;
-	
+	//main method for action handler
 	public EndTurnHandler(Game box) {
 		game = box;
 	}
