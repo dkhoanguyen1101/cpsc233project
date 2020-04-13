36277ad5ebda1963b0c379abe6184721d27dd7cd
yuwen-liang <60663219+yuwen-liang@users.noreply.github.com>
2020-04-13 14:42:08 -0600

    Update AttackHandler.java

--- gui-fixload/AttackHandler.java
+++ gui-fixload/AttackHandler.java
@@ -7,16 +7,16 @@ import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.control.Label;
-
+//method for attack handler by normal attack
 public class AttackHandler implements EventHandler<ActionEvent>{
-	
+	//instance variable
 	Game game;
 	Chara attacker;
-	
+	//constructor for attack handler
 	public AttackHandler(Game box, Chara a) {
 		game = box; attacker = a;
 	}
-	
+	//main method for attack event
 	public void handle(ActionEvent event) {
 		
 		game.setButtonTextAttack();
