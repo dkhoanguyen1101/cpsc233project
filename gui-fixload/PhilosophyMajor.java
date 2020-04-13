1375aaf9d081e4b965591f742c5bb06e191998e6
robert618 <59930386+robert618@users.noreply.github.com>
2020-04-13 11:04:03 -0600

    Description fix

--- gui-fixload/PhilosophyMajor.java
+++ gui-fixload/PhilosophyMajor.java
@@ -7,7 +7,7 @@ public class PhilosophyMajor extends Chara {
 	
 	//constructor
 	public PhilosophyMajor(int id) {
-		super("Philosophy Major", id, 15, 200, 2, 15, 200, 5, 5, 2, "This special lowers the attack \n of a chosen enemyn (costs 2 mana). \n Select the tile of the enemy \n you want to target", "Philosophy.png");
+		super("Philosophy Major", id, 15, 200, 2, 15, 200, 5, 5, 2, "This special lowers the attack \n of a chosen enemy (costs 2 mana). \n Select the tile of the enemy \n you want to target", "Philosophy.png");
 	}
 	
 	//methods
