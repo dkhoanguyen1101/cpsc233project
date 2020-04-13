a8c50ff6164e970dd21e9602b83d8da0066e3467
yuwen-liang <60663219+yuwen-liang@users.noreply.github.com>
2020-04-13 11:01:11 -0600

    Add files via upload

---
+++ gui-save and load/AttackHandler.java
@@ -0,0 +1,30 @@
+
+
+import javafx.event.ActionEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.control.Label;
+
+public class AttackHandler implements EventHandler<ActionEvent>{
+	
+	Game game;
+	Chara attacker;
+	
+	public AttackHandler(Game box, Chara a) {
+		game = box; attacker = a;
+	}
+	
+	public void handle(ActionEvent event) {
+		
+		game.setButtonTextAttack();
+		game.btn1.setOnAction(null);
+		game.btn2.setOnAction(null);
+		game.btn3.setOnAction(null);
+		game.btn4.setOnAction(new GoBackHandler(game));
+		game.scene.setOnMouseClicked(new AttackChooseEnemyHandler(game, attacker));
+	}
+
+}
