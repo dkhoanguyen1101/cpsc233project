fb63d27f51e2b471fd7b6861a6be180cbe7c8c6a
yuwen-liang <60663219+yuwen-liang@users.noreply.github.com>
2020-04-13 14:48:21 -0600

    Update SaveHandler.java

--- gui-fixload/SaveHandler.java
+++ gui-fixload/SaveHandler.java
@@ -4,20 +4,22 @@ import java.io.ObjectOutputStream;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-
+//method for What happens when you press the save button
 public class SaveHandler implements EventHandler<ActionEvent> {
-
+//instance variable
     Game game;
-
+//constructor
     public SaveHandler(Game box) {
         game = box;
     }
-
+//main method for save and use try catch exception
     public void handle(ActionEvent event) {
 
           try {
-        	  ObjectOutputStream output = new ObjectOutputStream(new FileOutputStream("output.bin"));//create save file
-        	  output.writeObject(game.players);//write required data to save file
+              //create save file
+        	  ObjectOutputStream output = new ObjectOutputStream(new FileOutputStream("output.bin"));
+              //write required data to save file
+        	  output.writeObject(game.players);
         	  output.writeObject(game.enemies);
         	  output.writeObject(game.notMoved);
         	  output.writeObject(game.notActed);
@@ -26,7 +28,8 @@ public class SaveHandler implements EventHandler<ActionEvent> {
         	  output.writeObject(game.getGPA());
         	  output.writeObject(game.turnNo);
         	  output.writeObject(game.mapNo);
-        	  output.close();//close when finished
+              //close when finished
+        	  output.close();
 
         } catch (IOException io) {
             io.printStackTrace();
