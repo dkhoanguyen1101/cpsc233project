97289514fcf26add7738013c708fba237c31d65d
yuwen-liang <60663219+yuwen-liang@users.noreply.github.com>
2020-04-13 14:45:30 -0600

    Update ItemHandler.java

--- gui-fixload/ItemHandler.java
+++ gui-fixload/ItemHandler.java
@@ -7,16 +7,16 @@ import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.control.Label;
-
+//method for item handler
 public class ItemHandler implements EventHandler<ActionEvent>{
-	
+	//instance variable
 	Game game;
 	Chara user;
-	
+	//constructor
 	public ItemHandler(Game box, Chara u) {
 		game = box; user = u;
 	}
-	
+	//main method for item handler
 	public void handle(ActionEvent event) {
 		game.setButtonTextItems(user.getID());
 		game.btn1.setOnAction(new ItemChooseUseHandler(game, user, 0));
