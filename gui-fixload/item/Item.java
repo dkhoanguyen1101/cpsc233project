0d93ed53fcf4c75f206e702cbea2346c59ca5bbb
robert618 <59930386+robert618@users.noreply.github.com>
2020-04-14 10:14:05 -0600

    Update Item.java

--- gui-fixload/item/Item.java
+++ gui-fixload/item/Item.java
@@ -7,10 +7,14 @@ public abstract class Item implements Serializable {
 private  int ID;
 private int staticID;
 private String name, imageURL;
+
+//constructor for item class
 public Item(int id, String aName) {
 	setID(id);
 	setName(aName);
 	imageURL = "Item.png";
+	
+//getters and setters
 }
 public int getStatic() {
 	int toReturn = staticID;
@@ -36,6 +40,7 @@ public String getImageURL() {
 	return new String(imageURL);
 }
 
+//abstract method to be used by other items
 public abstract boolean use(Chara c);
 
 
