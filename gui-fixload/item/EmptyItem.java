be2b73bcbf5edf0498dcee1f805f9f576c47a9ed
robert618 <59930386+robert618@users.noreply.github.com>
2020-04-14 10:03:23 -0600

    Update EmptyItem.java

--- gui-fixload/item/EmptyItem.java
+++ gui-fixload/item/EmptyItem.java
@@ -2,6 +2,7 @@ package item;
 
 import characters.Chara;
 
+//this class is a place holder for when a character is not holding an item
 public class EmptyItem extends Item {
 	public EmptyItem() {
 		super(31, "Empty");
